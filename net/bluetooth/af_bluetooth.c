df4faf276cab2530b14df6191fe0e902f8dc0a3a
Pavle Radosavljevic <pavlepiramida@gmail.com>
2017-04-29 01:42:43 +0200

    squash 3.0.75-76

--- net/bluetooth/af_bluetooth.c
+++ net/bluetooth/af_bluetooth.c
@@ -229,6 +229,8 @@ int bt_sock_recvmsg(struct kiocb *iocb, struct socket *sock,
 
 	if (flags & (MSG_OOB))
 		return -EOPNOTSUPP;
+	
+	msg->msg_namelen = 0;
 
 	skb = skb_recv_datagram(sk, flags, noblock, &err);
 	if (!skb) {
